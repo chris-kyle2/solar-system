1e2be6908e17ead83f8fbc985e8c18c6b0407037
chris-kyle2 <22ms0010@iitism.ac.in>
2025-03-27 17:23:18 +0530

    test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors());
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://mongo:27017/testdb";
+const MONGO_URI ="mongodb://mongo:27017/testdb";
 
 async function connectDB() {
     try {
